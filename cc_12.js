e5fefb74a2e284d659dc3db503725a41f9ddbb12
Maidele Archelus <maidele@usf.edu>
2025-03-02 19:53:31 -0500

    Task 1 - Created Revenue Metric Card

---
+++ cc_12.js
@@ -0,0 +1,8 @@
+//Task 1
+const dashboard = document. getElementById("dashboard");
+const dashboardQuery = document.querySelector ("#dashboard");
+const revenueCard = document.createElement("div");
+revenueCard.setAttribute("class", "metric-card");
+revenueCard.setAttribute("id", "revenue-card");
+
+
