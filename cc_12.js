ef5a827b4ed47a7ee354b9afc2b4270344ba8053
Maidele Archelus <maidele@usf.edu>
2025-03-02 21:53:16 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -4,5 +4,15 @@ const dashboardQuery = document.querySelector ("#dashboard");
 const revenueCard = document.createElement("div");
 revenueCard.setAttribute("class", "metric-card");
 revenueCard.setAttribute("id", "revenue-card");
+dashboard.appendChild(revenueCard)
+
+//Task 2
+const metricCards = document.querySelectorAll(".metric-card");
+const metricCardArray = Array.from (metricCards);
+
+metricCardArray.forEach(card => {
+    card.text += " - Updated";
+    card.style.greenbackgroundColor = 'green';
+})
 
 
